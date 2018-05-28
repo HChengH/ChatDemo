ac2d5f74fe639f6af6d8c7b48c9e4c6c83c37b44
He <chenghe1996@163.com>
2018-05-28 12:22:43 +0800

    add dummy

---
+++ chatDemo/chatDemo_dummy.m
@@ -0,0 +1,15 @@
+//
+//  chatDemo+dummy.m
+//  chatDemo
+//
+//  Created by 翯 on 2018-05-28.
+//  Copyright © 2018 翯. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface chatDemo_dummy : NSObject
+@end
+
+@implementation chatDemo_dummy
+@end
